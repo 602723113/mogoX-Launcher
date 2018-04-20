d666d6389eb1ae65011cc6a883aa6c4ef1e583b9
Zoyn <602723113@qq.com>
2018-04-20 23:58:21 +0800

    update: 更新图标相关内容, 还有签名好像忘记上传了

--- src/cc/zoyn/mogox/Main.java
+++ src/cc/zoyn/mogox/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -19,22 +20,30 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
+
+        // 配置文件检查
+        checkIsFirstRun();
+
         Parent root = FXMLLoader.load(getClass().getResource("fxml/gui.fxml"));
         primaryStage.setTitle("mogoX 启动器");
-
         // 标题栏设置
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         Scene scene = new Scene(root, 638, 400);
         AnchorPane title = (AnchorPane) scene.lookup("#mainTitleBar");
 
         primaryStage.setScene(scene);
-        primaryStage.setMaximized(false);
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("resource/icon.jpg")));
+
         DragUtil.addDragListener(primaryStage, title);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void checkIsFirstRun() {
+
     }
 
     public static String getJavaDirectory() {
---
+++ src/cc/zoyn/mogox/util/CommonUtils.java
@@ -0,0 +1,67 @@
+package cc.zoyn.mogox.util;
+
+import cc.zoyn.mogox.Main;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+
+/**
+ * 通用 工具类
+ *
+ * @author Zoyn
+ */
+public class CommonUtils {
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+
+    public static String getClientPath() {
+        String filePath = "";
+        URL url = Main.class.getProtectionDomain().getCodeSource().getLocation();
+        try {
+            // 转化为utf-8编码，支持中文
+            filePath = URLDecoder.decode(url.getPath(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 可执行jar包运行的结果里包含".jar"
+        if (filePath.endsWith(".jar")) {
+            // 获取jar包所在目录
+            filePath = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+        }
+
+        File file = new File(filePath);
+        filePath = file.getAbsolutePath();
+        return filePath;
+    }
+
+    public static void saveJavaPath(String javaPath) {
+        File file = new File(getClientPath(), "config.json");
+        if (file.exists()) {
+
+        } else {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            JsonObject object = new JsonObject();
+            object.addProperty("javaPath", javaPath);
+            System.out.println(GSON.toJson(object));
+            try {
+                FileWriter writer = new FileWriter(file);
+                writer.write(object.toString());
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
